50eed7253dcec930d9289f919861a20b33d19e0b
Mrinal <mail.mrinal10@gmail.com>
2024-08-24 00:33:48 +0530

    message

--- src/component/Tabs/Tabs.js
+++ src/component/Tabs/Tabs.js
@@ -4,24 +4,20 @@ import axios from 'axios';
 import './Tabs.css';
 
 
-
-
-
-
 const Tabs = () => {
-  const [activeTab, setActiveTab] = useState('Tab1');
-
-  const handleTabClick = (tabName) => {
+  const [activeTab, setActiveTab] = useState('Tab1'); // default to active tab
+  const [teams, setTeams] = useState([]);
+  const handleTabClick = async (tabName) => {
+    console.log(tabName);
     setActiveTab(tabName);
-  };
-
-  const handleFootballData = async (e) => {
-    
-    try {
-      const response = await axios.get('http://localhost:5000/api/data');
-      console.log(response.getbody());
-    } catch (error) {
-      console.error("error getting football data");
+    if(tabName === 'Tab2') {
+      try {
+        const response = await axios.get('http://localhost:5000/api/data');
+        setTeams(response.data);
+        console.log(JSON.stringify(response, null, 2));
+      } catch (error) {
+        console.error("error getting football data" + error);
+      }
     }
   };
 
@@ -31,32 +27,26 @@ const Tabs = () => {
         <button
           className={activeTab === 'Tab1' ? 'active' : ''}
           onClick={() => handleTabClick('Tab1')}
-        >
-           Tab 1
-        </button>
+        >Tab 1</button>
         <button
           className={activeTab === 'Tab2' ? 'active' : ''}
-          onClick={() => {handleTabClick('Tab2'); handleFootballData();}}>
-        Football</button>
-        <button
-          className={activeTab === 'Tab3' ? 'active' : ''}
-          onClick={() => handleTabClick('Tab3')}
-        >
-          Tab 3
-        </button>
-        <button
-          className={activeTab === 'Tab4' ? 'active' : ''}
-          onClick={() => handleTabClick('Tab4')}
-        >
-          Tab 4
-        </button>
+          onClick={() => {handleTabClick('Tab2'); }}>Football</button>
       </div>
 
       <div className="tab-content">
         {activeTab === 'Tab1' && <div>Content for Tab 1</div>}
-        {activeTab === 'Tab2' && <div>Content for Tab 2</div>}
-        {activeTab === 'Tab3' && <div>Content for Tab 3</div>}
-        {activeTab === 'Tab4' && <div>Content for Tab 4</div>}
+        {activeTab === 'Tab2' && 
+        <div className="team-list">
+          <h2>Teams</h2>
+          <div className="team-container">
+              {teams.map(team => (
+                <div className="team-box" key={team.team_name}>
+                  {team.team_name} - {team.team_location}
+                  {team.team_stadium}
+                </div>
+              ))}
+          </div>
+        </div>}
       </div>
     </div>
   );
